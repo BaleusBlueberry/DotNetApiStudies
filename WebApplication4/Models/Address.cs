476be35cc6bdf8eca488fd66a5e3de25e3d277e9
BaleusBlueberry <ssaasshh29@gmail.com>
2024-07-24 18:18:33 +0300

    lesson 4 and 5

---
+++ WebApplication4/Models/Address.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication4.Models
+{
+    public class Address
+    {
+        public string? State { get; set; }
+
+        [Required(ErrorMessage = "Country is required")]
+        public required string Country { get; set; }
+
+        [Required(ErrorMessage = "City is required")]
+        public required string City { get; set; }
+
+        [Required(ErrorMessage = "Street is required")]
+        public required string Street { get; set; }
+
+        [Required(ErrorMessage = "HouseNumber is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "house number must be positive")]
+        public int HouseNumber { get; set; }
+
+        
+        public int Zip { get; set; } = 0;
+    }
+}
---
+++ WebApplication4/Models/Image.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication4.Models
+{
+    public class Image
+    {
+        [Required(ErrorMessage = "Url is required")]
+        [Range(14, int.MaxValue, ErrorMessage = "Url Link must be more then 14 characters")]
+        public string Url { get; set; }
+
+        [Required(ErrorMessage = "Alt is required")]
+        [Range(2, 256, ErrorMessage = "Alt must be between 2 and 256 characters")]
+        public string Alt { get; set; }
+    }
+}
